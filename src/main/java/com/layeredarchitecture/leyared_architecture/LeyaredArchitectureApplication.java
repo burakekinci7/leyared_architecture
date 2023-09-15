fcf98db4aa3b0e43cf48d5707f92ec0d7c4cad87
burakekinci7 <ekincib015@gmail.com>
2023-09-15 09:37:30 +0300

    Initial commit Created Project

---
+++ src/main/java/com/layeredarchitecture/leyared_architecture/LeyaredArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.layeredarchitecture.leyared_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeyaredArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyaredArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/layeredarchitecture/leyared_architecture/LeyaredArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.layeredarchitecture.leyared_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeyaredArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
